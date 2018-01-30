3c196579aae4f1d12e84f36f5e23ff9eca3fed9a
must <mustapha.benosmane@gmail.com>
2018-01-30 22:48:06 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"strings"
+    "encoding/json"
+    "log"
+    "net/http"
+	"github.com/gorilla/mux"
+	"github.com/google/uuid"
+)
+
+type Mock struct {
+    UUID    string   `json:"uuid,omitempty"`
+	Message string   `json:"message,omitempty"`
+	Headers []HttpHeader `json:"headers,omitempty"`
+}
+
+type HttpHeader struct {
+	Value string `json:"value,omitempty"`
+}
+
+func GetMock(w http.ResponseWriter, r *http.Request) {
+	var head []HttpHeader
+	for key, value := range r.Header {		
+		head = append(head, HttpHeader{Value: strings.Join([]string{key, strings.Join(value," ")}, ":")})
+	}
+	var mock Mock = Mock{UUID: uuid.New().String(), Message: "Hello world !", Headers: head}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)	
+    json.NewEncoder(w).Encode(mock)
+}
+
+// our main function
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/mock", GetMock).Methods("GET")
+	log.Fatal(http.ListenAndServe(":80", router))
+	return
+}
