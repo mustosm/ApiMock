8b86913ddd7012c2fcf4b6ccddc5ca16bf5eef68
mustosm <mustapha.benosmane@gmail.com>
2018-06-26 23:27:24 +0200

    remove oauth

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"encoding/pem"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -13,12 +12,10 @@ import (
 	"strings"
 	"time"
 
-	"github.com/auth0-community/auth0"
 	"github.com/google/uuid"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/magiconair/properties"
-	jose "gopkg.in/square/go-jose.v2"
 )
 
 type Mock struct {
@@ -73,41 +70,6 @@ func LoadPublicKey(data []byte) (interface{}, error) {
 	}
 
 	return nil, fmt.Errorf("square/go-jose: parse error, got '%s' and '%s'", err0, err1)
-}
-
-/* Set up a global string for our secret */
-var mySigningKey = []byte("secret")
-var validator *auth0.JWTValidator
-
-func authMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//Creates a configuration with the Auth0 information
-		data, err := ioutil.ReadFile("mustosm.pem")
-		if err != nil {
-			panic("Impossible to read key form disk")
-		}
-
-		secret, err := LoadPublicKey(data)
-		if err != nil {
-			panic("Invalid provided key")
-		}
-		audience := []string{"https://api.mustosm.io/v1/myFirstAPI"}
-		secretProvider := auth0.NewKeyProvider(secret)
-		configuration := auth0.NewConfiguration(secretProvider, audience, "https://mustosm.eu.auth0.com/", jose.RS256)
-		validator = auth0.NewValidator(configuration)
-
-		token, err := validator.ValidateRequest(r)
-
-		if err != nil {
-			fmt.Println(err)
-			fmt.Println("Token is not valid:", token)
-			w.WriteHeader(http.StatusUnauthorized)
-			w.Write([]byte("Unauthorized"))
-		} else {
-			next.ServeHTTP(w, r)
-		}
-
-	})
 }
 
 func main() {
